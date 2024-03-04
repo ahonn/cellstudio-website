d92ac3d24114a59f3d410abe347092c8f8c91bf9
ahonn <ahonn95@outlook.com>
2024-03-04 18:26:17 +1100

    feat: fix typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.NODE_ENv === 'production' ? 'https://cell.studio' : 'http://localhost:4321',
+  site: process.env.NODE_ENV === 'production' ? 'https://cell.studio' : 'http://localhost:4321',
   integrations: [tailwind()],
 });
